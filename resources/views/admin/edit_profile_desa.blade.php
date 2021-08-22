863a2fbc75be9ed4b5bea823b75d647f82084737
Dasep12 <depiyawandasep13@gmail.com>
2021-08-22 13:40:50 +0700

    upload profile db

--- resources/views/admin/edit_profile_desa.blade.php
+++ resources/views/admin/edit_profile_desa.blade.php
@@ -36,33 +36,6 @@
                         {{ $data->profile }}
                         </textarea>
                     </div>
-<<<<<<< HEAD
                 </div>
-                <div class="ibox-content no-padding">
-                    <form method="post" action="" enctype="multipart/form-data">
-                        @csrf
-                        <div class="summernote">
-                            <h3>Lorem Ipsum is simply</h3>
-                            dummy text of the printing and typesetting industry. <strong>Lorem Ipsum has been the industry's</strong> standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
-                            typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
-                            <br />
-                            <br />
-                            <ul>
-                                <li>Remaining essentially unchanged</li>
-                                <li>Make a type specimen book</li>
-                                <li>Unknown printer</li>
-                            </ul>
-                        </div>
-                </div>
-                </form>
-            </div>
-=======
-                    <div class="form-group mt-2">
-                        <button type="submit" class="btn btn-info">Simpan Data</button>
-                    </div>
-            </form>
->>>>>>> author
         </div>
-    </div>
-    @endsection
+        @endsection
